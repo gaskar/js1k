926d5a30c31a79ff9692ee98b0f1446dfd8eb9ef
gaskar <kagasparyan@gmail.com>
2014-02-18 15:07:55 +0400

    added hobbit element, now size is 1056bayt

--- app/demo.js
+++ app/demo.js
@@ -19,32 +19,29 @@ hobbit.prototype.move = function(){
   c.beginPath();
   c.arc(this.x, this.y, radius, 0, 2 * Math.PI, false);
   c.fillStyle = 'green';
-  c.fill();
-  c.closePath();
+  var startingX = this.x, startingY = this.y;
+  var startingPoints = [[ startingX, startingY ], [ startingX - 24, startingY ], [ startingX - 1, startingY + 1 ], [ startingX - 46, startingY - 46 ], [ startingX - 46, startingY - 46 ], [ startingX + 10, startingY - 2 ], [ startingX - 46, startingY - 47 ], [ startingX - 25, startingY - 33 ], [ startingX - 46, startingY - 47 ], [ startingX - 48, startingY - 117 ], [ startingX - 48, startingY - 117 ], [ startingX - 48, startingY - 117 ], [ startingX - 48, startingY - 117 ], [ startingX - 5, startingY - 159 ], [ startingX - 5, startingY - 159 ], [ startingX - 4, startingY - 120 ], [ startingX + 42, startingY - 58 ], [ startingX + 42, startingY - 58 ], [ startingX - 82, startingY - 56 ], [ startingX - 82, startingY - 55 ]], bezierPoints = [[ startingX + 99, startingY - 20, startingX + 79, startingY + 17, startingX - 2, startingY ], [ startingX - 114, startingY - 22, startingX - 96, startingY + 20, startingX - 24, startingY ], [ startingX + 7, startingY - 40, startingX - 39, startingY - 16, startingX - 24, startingY ], [ startingX - 57, startingY - 33, startingX - 45, startingY - 6, startingX - 33, startingY - 2 ], [ startingX - 57, startingY - 33, startingX + 2, startingY - 27, startingX + 15, startingY - 40 ], [ startingX + 28, startingY - 18, startingX + 26, startingY - 29, startingX + 15, startingY - 40 ], [ startingX - 17, startingY - 35, startingX + 2, startingY - 40, startingX + 12, startingY - 47 ], [ startingX - 36, startingY - 45, startingX + 18, startingY - 39, startingX - 7, startingY - 34 ], [ startingX - 13, startingY - 124, startingX + 17, startingY - 38, startingX + 16, startingY - 41 ], [ startingX - 53, startingY - 93, startingX + 6, startingY - 45, startingX - 3, startingY - 119 ], [ startingX - 46, startingY - 97, startingX - 5, startingY - 75, startingX - 3, startingY - 119 ], [ startingX - 44, startingY - 130, startingX - 16, startingY - 145, startingX - 3, startingY - 119 ], [ startingX - 51, startingY - 142, startingX - 42, startingY - 141, startingX - 40, startingY - 146 ], [ startingX - 14, startingY - 172, startingX - 38, startingY - 151, startingX - 40, startingY - 144 ], [ startingX, startingY - 165, startingX - 30, startingY - 156, startingX - 8, startingY - 140 ], [ startingX + 5, startingY - 137, startingX - 22, startingY - 146, startingX - 8, startingY - 139 ], [ startingX + 36, startingY - 59, startingX + 18, startingY - 78, startingX - 12, startingY - 78 ], [ startingX + 39, startingY - 41, startingX + 38, startingY - 54, startingX + 6, startingY - 60 ], [ startingX - 73, startingY - 38, startingX - 70, startingY - 48, startingX - 38, startingY - 65 ], [ startingX - 57, startingY - 63, startingX - 35, startingY - 80, startingX - 21, startingY - 78 ]], length = 20;
+  for( var i = 0; i < length; i++ ) {
+    c.moveTo( startingPoints[i][0], startingPoints[i][1] );
+    c.bezierCurveTo( bezierPoints[i][0], bezierPoints[i][1], bezierPoints[i][2], bezierPoints[i][3], bezierPoints[i][4], bezierPoints[i][5] );
+  }
   c.stroke();
 }
 
 var es = [];
 for(i = 0; i < 5; i++)
   es[i] = new hobbit({x : (i + 1) * 50, y : 0});
-d=e=f=h=0;
-i=j=300;
+
 var n = function() {
-  p="fEFf&{{~_=,;=vviJ.jfVi/.OoyizyhkhEwf74)\n$fwwuvtU`"+(10<h?"iZ[*)yj:*im**y|Ktdww54#5Dy\iz[Kzi[Jiijk[e@1!":"zl]LfU{\lKtBUh{zzU66iigig5\n&iiyz{vfwwiyDfwiiE");
-  c.fillStyle="#000";
-  c.beginPath();
-  c.moveTo(v=i-e,w=(j-=h)-e);
-  for(o in p){
-    y=8-2*(p.charCodeAt(o)>>4);
-    z=16-2*(p.charCodeAt(o)&15);
-    c.lineTo(v+=(e?y:z),w+=(e?-z:y));
-  }
+
   c.fill();
   c.beginPath();
   c.fillStyle="#FFF";
 
   c.fillRect(0,0,4E4,4E4);
   c.fill();
+
+
 
   for(i = 0; i < 5; i++)
     es[i].move();
@@ -63,5 +60,3 @@ a.onclick = function(e) {
 }
 
 n();
-
-4
