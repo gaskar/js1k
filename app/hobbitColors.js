7b585261846bed939d12d4ee2d041fb69df16904
shooshanik <shusho92@gmail.com>
2014-02-22 00:30:50 +0400

    Added new dragon, changed head of hobbit.

--- app/hobbitColors.js
+++ app/hobbitColors.js
@@ -4,13 +4,16 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
 /*smallest functions for drawing dragins and hobbit :(( */
-function drawDragon(context){
+function drawDragon2(context){
     var startingPoints = [[82,30], [82,30], [65,28], [65,28], [53,32], [53,32], [68,33], [67,34], [71,21], [71,21], [69,22]],
         bezierPoints = [[83,28,89,28,89,31 ], [83,28,78,27,76,28], [63,20,77,20,76,28], [63,24,57,28,59,29], [52,29,56,27,59,29], [48,27,75,13,89,30], [73,28,65,26,69,22], [74,26,72,24,70,22], [70,20,69,21,70,22], [70,15,70,18,68,20], [69,20,69,21,68,20]], length = 11;
+    context.beginPath();
+    context.fillStyle="#CC0000";
     for( var i = 0; i < length; i++ ) {
         context.moveTo( 10*startingPoints[i][0], 10*startingPoints[i][1] );
         context.bezierCurveTo( 10*bezierPoints[i][0], 10*bezierPoints[i][1], 10*bezierPoints[i][2], 10*bezierPoints[i][3], 10*bezierPoints[i][4], 10*bezierPoints[i][5] );
     }
+    context.fill();
     context.stroke();
 }
 
@@ -78,32 +81,83 @@ function hardcodedHobbit(context){
 
     /*head*/
     context.beginPath();
-    context.moveTo( 257, 135 );
-    context.bezierCurveTo( 284, 216, 191, 246, 191, 137 );
+    /*context.moveTo( 257, 135 );
+    context.bezierCurveTo( 284, 216, 191, 246, 191, 137 );*/
+    context.moveTo( 247, 176 );
+    context.bezierCurveTo( 263, 215, 191, 246, 198, 157 );
+    context.fill();
+    context.stroke();
+    context.beginPath();
+
+
+    context.moveTo( 183, 168 );
+    context.bezierCurveTo( 192, 173, 175, 185, 200, 193 );
+    context.moveTo( 183, 167 );
+    context.bezierCurveTo( 193, 173, 190, 179, 200, 180 );
     context.fill();
     context.stroke();
 
     /*hair*/
     context.beginPath();
-    context.strokeStyle="#CC9900";
-    context.lineWidth = 3;
-    context.moveTo( 181, 171 );
-    context.bezierCurveTo( 160, 122, 213, 162, 190, 135 );
+    context.fillStyle="#CC9900";
 
-    context.moveTo( 203, 162 );
-    context.bezierCurveTo( 232, 136, 202, 147, 200, 130 );
+    context.moveTo( 197, 177 );
+    context.bezierCurveTo( 183, 170, 193, 145, 201, 158 );
 
-    context.moveTo( 248, 161 );
-    context.bezierCurveTo( 237, 114, 213, 171, 214, 123 );
+    context.moveTo( 257, 173 );
+    context.bezierCurveTo( 237, 181, 197, 177, 198, 149 );
 
-    context.moveTo( 290, 173 );
-    context.bezierCurveTo( 224, 190, 278, 138, 232, 122 );
+    context.moveTo( 257, 173 );
+    context.bezierCurveTo( 261, 159, 218, 133, 198, 149 );
 
-    context.moveTo( 276, 189 );
-    context.bezierCurveTo( 242, 176, 291, 143, 259, 135 );
     context.closePath();
+    context.fill()
+
+}
+function hardcodedDragon(context){
+    context.beginPath();
+    context.strokeStyle="#8D1919";
+
+    context.moveTo( 615, 207 );
+    context.bezierCurveTo( 769, 220, 864, 296, 908, 357 );
+
+    context.moveTo( 615, 207 );
+    context.bezierCurveTo( 584, 192, 633, 166, 641, 193 );
+
+    context.moveTo( 531, 189 );
+    context.bezierCurveTo( 593, 117, 576, 106, 643, 193 );
+
+    context.moveTo( 531, 189 );
+    context.bezierCurveTo( 550, 168, 580, 194, 553, 205 );
+
+    context.moveTo( 326, 347 );
+    context.bezierCurveTo( 387, 255, 498, 217, 553, 205 );
+
+    context.moveTo( 326, 347 );
+    context.bezierCurveTo( 535, 172, 376, 419, 538, 284 );
+
+    context.moveTo( 562, 223 );
+    context.bezierCurveTo( 549, 231, 497, 269, 538, 284 );
+
+    context.moveTo( 562, 223 );
+    context.bezierCurveTo( 539, 347, 640, 255, 652, 407 );
+
+    context.moveTo( 612, 228 );
+    context.bezierCurveTo( 576, 343, 671, 246, 652, 407 );
+
+    context.moveTo( 612, 228 );
+    context.bezierCurveTo( 576, 343, 671, 246, 652, 407 );
+
+    context.moveTo( 612, 228 );
+    context.bezierCurveTo( 639, 245, 668, 272, 645, 283 );
+
+    context.moveTo( 907, 355 );
+    context.bezierCurveTo( 550, 114, 819, 380, 645, 283 );
+
+    context.closePath();
+    context.fill();
     context.stroke();
 }
 
-
 hardcodedHobbit(context);
+//hardcodedDragon(context);
