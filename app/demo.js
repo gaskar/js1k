c684799469f56051b01f5d5e0b41b03b71269ab3
gaskar <gaskar18@gmail.com>
2014-03-31 00:09:44 +0400

    added counter and failing.

--- app/demo.js
+++ app/demo.js
@@ -1,20 +1,32 @@
 var a = document.getElementById('thecanvas');
-var b = document.body;
+a.width = window.innerWidth;
+a.height = window.innerHeight;
+var h = a.height;
 var c = a.getContext('2d');
+
+var q=0;
+c.scale(a.height/1000,a.height/1000)
+c.font = '100pt Courier';
+          
 
 var i = 0,radius = 200,hobbits = [[Math.random() * 700, 0, .7], [Math.random() * 700, 0, .5], [Math.random() * 700, 0, 1.3], [Math.random() * 700, 0, 1.7], [Math.random() * 700,0, 1.9]]
     ,n = function() {
-
+    
         c.beginPath();
-        c.fillStyle="#FFF";
+        c.fillStyle="#303030";  
+       /* c.fillStyle = grd;*/
         c.fillRect(0,0,4E4,4E4);
         c.fill();
         for(i in hobbits){
+            if(hobbits[i][1]>=h){
+                n=undefined;
+            }
             hardcodedHobbit(c,hobbits[i][0] ,hobbits[i][1] = hobbits[i][1] + hobbits[i][2]);
             drawHair(c,hobbits[i][0] ,hobbits[i][1] = hobbits[i][1] + hobbits[i][2]);
         }
         hardcodedDragon(c,-220,670);
-        setTimeout(n, 16);
+        c.fillText(q, h*0.7, h*0.9);
+        setTimeout(n, 16);      
     };
 var m=function(x,y,i){
     var k = 1;
@@ -30,10 +42,8 @@ a.onclick = function(e) {
     for(var i in hobbits)
     {
         if(0 < parseInt(e.x - hobbits[i][0]) && parseInt(e.x - hobbits[i][0]) < 180 && 0 < (e.y - hobbits[i][1]) && (e.y - hobbits[i][1]) <= 200){
-
-
+            q++;
             m(hobbits[i][0],hobbits[i][1],i);
-
             setTimeout(function() {
                 hobbits[i][1] = -radius*2;
                 hobbits[i][2] += 0.3;
@@ -82,7 +92,8 @@ function hardcodedHobbit(c,xoff, yoff){
 
 function hardcodedDragon(c,xoff, yoff){
     c.beginPath();
-    c.fillStyle="#8D1919";
+    // c.fillStyle="#8D1919";
+    c.fillStyle="4D0000";
     c.moveTo( 967 + xoff, 305 + yoff );
     var convertedStr = 'ίá͂¥˔£ʞ§ʴrˎʣUʡWɳʆxʜɹ£Ⱥ¥ǏÕƽĝɌyȤĸɸâɦàɡ¿ʈ³ɿđʿ¿ʨļ˫óʜîʹµ˘¿˫×˔Þ̿ĺ˳[φİ ';
     for(var i = 0; i < convertedStr.length; i+=6) {
@@ -98,6 +109,25 @@ function hardcodedDragon(c,xoff, yoff){
     c.stroke();
 }
 
+
+
+function hardcodedDragonfa(c,xoff, yoff){
+    c.beginPath();
+    c.fillStyle="#8D1919";
+    c.moveTo( 967 + xoff, 305 + yoff );
+    var convertedStr = 'ίá͂¥˔£ʞ§ʴrˎʣUʡWɳʆxʜɹ£Ⱥ¥ǏÕƽĝɌyȤĸɸâɦàɡ¿ʈ³ɿđʿ¿ʨļ˫óʜîʹµ˘¿˫×˔Þ̿ĺ˳[φİ ';
+    for(var i = 0; i < convertedStr.length; i+=6) {
+        console.log('c.bezierCurveTo(' + convertedStr.charCodeAt(i) + ' + xoff,\ ' +
+                convertedStr.charCodeAt(i + 1) + yoff,
+                convertedStr.charCodeAt(i + 2) + xoff,
+                convertedStr.charCodeAt(i + 3) + yoff,
+                convertedStr.charCodeAt(i + 4)+ xoff,
+                convertedStr.charCodeAt(i + 5)+ yoff);
+    }
+    c.closePath();
+    c.fill();
+    c.stroke();
+}
 
 
 
