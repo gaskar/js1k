6e1e3365eb8429dd049a9e60462ba8784aa41435
gaskar <gaskar18@gmail.com>
2014-03-31 03:00:21 +0400

    changed strings

--- app/converter.js
+++ app/converter.js
@@ -1,70 +1,66 @@
 var regexp = /\d+/g
-var nums = 'c.beginPath();\
-c.moveTo( 198 + xoff, 276 + yoff );\
-c.bezierCurveTo( 153 + xoff, 205 + yoff, 291 + xoff, 172 + yoff, 255 + xoff, 274 + yoff );\
-c.fill();\
-c.stroke();\
+var nums = 'c.moveTo( 49 + xoff, 88 + yoff );\
+    c.bezierCurveTo( 26 + xoff, 52 + yoff, 95 + xoff, 36 + yoff, 77 + xoff, 87 + yoff );\
+    c.fill();\
+    c.stroke();\
 \
 \
-c.beginPath();\
-c.moveTo( 249 + xoff, 324 + yoff );\
-c.bezierCurveTo( 316 + xoff, 236 + yoff, 121 + xoff, 257 + yoff, 214 + xoff, 329 + yoff );\
+    c.beginPath();\
+    c.moveTo( 74 + xoff, 112 + yoff );\
+    c.bezierCurveTo( 108 + xoff, 68 + yoff, 10 + xoff, 78 + yoff, 57 + xoff, 114 + yoff );\
 \
-c.moveTo( 214 + xoff, 329 + yoff );\
-c.bezierCurveTo( 210 + xoff, 310 + yoff, 240 + xoff, 297 + yoff, 249 + xoff, 324 + yoff);\
-c.fill();\
-c.stroke();\
-\
-c.beginPath();\
-c.moveTo( 255 + xoff, 274 + yoff );\
-c.bezierCurveTo( 158 + xoff, 284 + yoff, 185 + xoff, 260 + yoff, 259 + xoff, 268 + yoff );\
+c.moveTo( 57 + xoff, 114 + yoff );\
+c.bezierCurveTo( 55 + xoff, 105 + yoff, 70 + xoff, 98 + yoff, 74 + xoff, 112 + yoff);\
 c.fill();\
 c.stroke();\
 \
 c.beginPath();\
-c.moveTo( 214 + xoff, 268 + yoff );\
-c.bezierCurveTo( 272 + xoff, 262 + yoff, 210 + xoff, 289 + yoff, 214 + xoff, 268 + yoff );\
+c.moveTo( 77 + xoff, 87 + yoff );\
+c.bezierCurveTo( 29 + xoff, 92 + yoff, 42 + xoff, 80 + yoff, 79 + xoff, 84 + yoff );\
 c.fill();\
 c.stroke();\
 \
 c.beginPath();\
-c.moveTo( 237 + xoff, 330 + yoff );\
-c.bezierCurveTo( 329 + xoff, 299 + yoff, 353 + xoff, 353 + yoff, 234 + xoff, 331 + yoff );\
-\
-c.moveTo( 224 + xoff, 329 + yoff );\
-c.bezierCurveTo( 111 + xoff, 305 + yoff, 130 + xoff, 361 + yoff, 224 + xoff, 331  + yoff);\
+c.moveTo( 57 + xoff, 84 + yoff );\
+c.bezierCurveTo( 86 + xoff, 81 + yoff, 55 + xoff, 94 + yoff, 57 + xoff, 84 + yoff );\
 c.fill();\
 c.stroke();\
 \
 c.beginPath();\
+c.moveTo( 68 + xoff, 115 + yoff );\
+c.bezierCurveTo( 114 + xoff, 99 + yoff, 125 + xoff, 125 + yoff, 67 + xoff, 115 + yoff );\
 \
-c.moveTo( 257 + xoff, 222  + yoff);\
-c.bezierCurveTo( 310 + xoff, 204 + yoff, 321 + xoff, 245 + yoff, 263 + xoff, 236 + yoff );\
-\
-c.moveTo( 205 + xoff, 220 + yoff );\
-c.bezierCurveTo( 212 + xoff, 273 + yoff, 184 + xoff, 275 + yoff, 190 + xoff, 233 + yoff );\
+c.moveTo( 62 + xoff, 114 + yoff );\
+c.bezierCurveTo( 5 + xoff, 102 + yoff, 15 + xoff, 125 + yoff, 62 + xoff, 115  + yoff);\
 c.fill();\
 c.stroke();\
+\
 c.beginPath();\
-c.moveTo( 247 + xoff, 176 + yoff );\
-c.bezierCurveTo( 263 + xoff, 215 + yoff, 191 + xoff, 246 + yoff, 198 + xoff, 157 + yoff );\
+c.moveTo( 78 + xoff, 61  + yoff);\
+c.bezierCurveTo( 105 + xoff, 52 + yoff, 110 + xoff, 72 + yoff, 81 + xoff, 68 + yoff );\
+\
+c.moveTo( 52 + xoff, 60 + yoff );\
+c.bezierCurveTo( 56 + xoff, 86 + yoff, 42 + xoff, 87 + yoff, 45 + xoff, 66 + yoff );\
 c.fill();\
-c.stroke();'.match(regexp);
+c.stroke();\
+\
+c.beginPath();\
+c.moveTo( 52 + xoff, 33 + yoff );\
+c.bezierCurveTo( 40 + xoff, 60 + yoff, 86 + xoff, 77+ yoff, 76 + xoff, 26 + yoff );'.match(regexp);
+
 console.log(nums);
 var str = '';
 
 
 var nums = [
-    "1", "600", "198", "276", "153", "205", "291", "172", "255", "274",
-    "2", "353", "249", "324", "316", "236", "121", "257", "214", "329", "214", "329", "210", "310", "240", "297", "249", "324",
-    "1", "630", "255", "274", "158", "284", "185", "260", "259", "268",
-    "1", "FC0", "214", "268", "272", "262", "210", "289", "214", "268",
-    "2", "EDA", "237", "330", "329", "299", "353", "353", "234", "331", "224", "329", "111", "305", "130", "361", "224", "331",
-    "2", "EDA", "257", "222", "310", "204", "321", "245", "263", "236", "205", "220", "212", "273", "184", "275", "190", "233",
-    "1", "EDA", "204", "167", "180", "221", "273", "255", "252", "153"];
+    "1", "600", "49", "88", "26", "52", "95", "36", "77", "87",
+    "2", "353",  "74", "112", "108", "68", "10", "78", "57", "114", "57", "114", "55", "105", "70", "98", "74", "112",
+    "1", "630", "77", "87", "29", "92", "42", "80", "79", "84",
+    "1", "FC0", "57", "84", "86", "81", "55", "94", "57", "84",
+    "2", "EDA", "68", "115", "114", "99", "125", "125", "67", "115", "62", "114", "5", "102", "15", "125", "62", "115",
+    "2", "EDA", "78", "61", "105", "52", "110", "72", "81", "68", "52", "60", "56", "86", "42", "87", "45", "66",
+    "1", "EDA", "52", "33", "40", "60", "86", "77", "76", "26"];
 
-["198", "276", "153", "205", "291", "172", "255", "274", "249", "324", "316", "236", "121", "257", "214", "329", "214", "329", "210", "310", "240", "297", "249", "324", "255", "274", "158", "284", "185", "260", "259", "268", "214", "268", "272", "262", "210", "289", "214", "268", "237", "330", "329", "299", "353", "353", "234", "331", "224", "329", "111", "305", "130", "361", "224", "331", "257", "222", "310", "204", "321", "245", "263", "236", "205", "220", "212", "273", "184", "275", "190", "233", "247", "176", "263", "215", "191", "246", "198", "157"]
-    [88, 166, 43, 95, 181, 62, 145, 164, 139, 214, 206, 126, 11, 147, 104, 219, 104, 219, 100, 200, 130, 187, 139, 214, 145, 164, 48, 174, 75, 150, 149, 158, 104, 158, 162, 152, 100, 179, 104, 158, 127, 220, 219, 189, 243, 243, 124, 221, 114, 219, 1, 195, 20, 251, 114, 221, 147, 112, 200, 94, 211, 135, 153, 126, 95, 110, 102, 163, 74, 165, 80, 123, 137, 66, 153, 105, 81, 136, 88, 47]
 var j = 1;
 for(var i = 0; i < nums.length; i += 2 +(j * 8)) {
     str += String.fromCharCode(parseInt(nums[i]));
--- app/demo.js
+++ app/demo.js
@@ -1,34 +1,31 @@
 var a = document.getElementById('thecanvas');
 var b = document.body;
 var c = a.getContext('2d');
-c.font = '100pt Curier';
-          
+
+
 var i = 0,r = 200,q=0,d=a.height,f = [1,1,1,1,1];
 for(i in f)
     f[i]=[Math.random() * 700, 0, .7];
 
 function n(){
-        c.beginPath();
-        c.fillStyle="#FFF";
-        c.fillRect(0,0,4E4,4E4);
-        c.fill();
-        for(i in f){
-             if(f[i][1]>=d){
-                n=undefined;
-            }
-            hh(f[i][0] ,f[i][1] = f[i][1] + f[i][2]),h(f[i][0] ,f[i][1] = f[i][1] + f[i][2],'?S/)YDQiN.e0q"j#b2i,kOM}V7\'V>S ',"#C90")
-        }
-        h(-220,670,'χıίá͂¥˔£ʞ§ʴrˎʣUʡWɳʆxʜɹ£Ⱥ¥ǏÕƽĝɌyȤĸɸâɦàɡ¿ʈ³ɿđʿ¿ʨļ˫óʜîʹµ˘¿˫×˔Þ̿ĺ˳[φİ ',"#8D1919");
-        c.fillText(q, d*0.7, d*0.9);
-        setTimeout(n, 16);
-    }
+    c.beginPath();
+    c.fillStyle="#FFF";
+    c.fillRect(0,0,4E4,4E4);
+    c.font = '6em Curier';
+    c.fill();
+    for(i in f)
+        f[i][1]>=d?n=undefined:1,hh(f[i][0] ,f[i][1] = f[i][1] + f[i][2]),h(f[i][0]-15,(f[i][1] = f[i][1] + f[i][2])-14,'.3&;,7C6!A"GD@T#C D1T0M:*"5.3 ',"#C90")
+    h(0,0,'JaHY=S2S-T/N2R-K-L)R*O-R)S#SX_%O b)Y(Y(V+U*^0V.b5[-[2U3V5X2Y=b5LJa',"#8D1919");
+    c.fillText(q, d*0.7, d*0.9);
+    setTimeout(n, 16);
+}
+
 function m(x,y){
     var k = 1;
     (function n() {
         h(x, y + 0.5, 'F h@_^Dq@tMhHYDNN]4k-o6`2D1=l(p,sZ  !&\'6*=$:>>1JBOHP*I ', '#E68A00');
         k++;
-        if(k < 25)
-            setTimeout(n, 16);
+        k<25?setTimeout(n, 16):1
     })();
 }
 
@@ -36,7 +33,7 @@ a.onclick = function(e) {
     for(var i in f)
         if(0 < e.x - f[i][0] && e.x - f[i][0] < 180 && 0 < (e.y - f[i][1]) && (e.y - f[i][1]) <= 200){
             q++;
-            m(f[i][0],f[i][1],i);
+            m(f[i][0],f[i][1]);
             f[i][1] = -r*2;
             f[i][2] += 0.3;
             f[i][0] = Math.random() * 700;
@@ -47,24 +44,24 @@ a.onclick = function(e) {
 n()
 
 function hh(x, y){
-    x-=140;
-    y-=140;
-    var s = '؀ÆĔÍģ¬ÿĒ͓ùńļìyāÖŉÖŉÒĶðĩùńذÿĒĜ¹ĄăČ࿀ÖČĐĆÒġÖČ໚íŊŉīššêŋàŉoıũàŋ໚āÞĶÌŁõćìÍÜÔđ¸ē¾é໚Ì§´Ýđÿü ';
+    x-=20;
+    y-=20;
+    var s = '؀1X4_$MW͓JplDN9r9r7iFbJpذMW]*POT࿀9TVQ7^9T໚Dsrc}}Cs>rf}>s໚N=i4nHQD4<8V*W-B໚4!(<VML';
     var j = 1;
     for(var i = 0; i < s.length; i += 2 +(j * 8)) {
         j = s.charCodeAt(i);
-        var color = s.charCodeAt(i + 1).toString(16);
+        p='#'+s.charCodeAt(i + 1).toString(16);
         c.beginPath();
-        c.fillStyle="#" + color;
-        for(var b = i + 2; b < i + (j * 8); b++) {
+        c.fillStyle=p;
+        for(b = i + 2; b < i + (j * 8); b++) {
             c.moveTo( s.charCodeAt(b++) + x, s.charCodeAt(b++) + y );
 
-            c.bezierCurveTo(s.charCodeAt(b++) + x,
-                    s.charCodeAt(b++) + y,
-                    s.charCodeAt(b++) + x,
-                    s.charCodeAt(b++) + y,
-                    s.charCodeAt(b++)+ x,
-                    s.charCodeAt(b)+ y);
+            c.bezierCurveTo(s.charCodeAt(b++)+x,
+                    s.charCodeAt(b++)+y,
+                    s.charCodeAt(b++)+x,
+                    s.charCodeAt(b++)+y,
+                    s.charCodeAt(b++)+x,
+                    s.charCodeAt(b)+y);
         }
 
         c.fill();
